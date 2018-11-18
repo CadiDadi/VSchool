cdddc276b725264d53637dadd841ccec392b8124
Scott <cadidadi@gmail.com>
2018-11-18 07:40:39 -0700

    20181118 test gitignore

---
+++ Assignments/Assignments/React/colored-boxes/README.md
@@ -0,0 +1,15 @@
+
+******* TO Start Project - 'npm start' in Terminal
+
+https://coursework.vschool.io/colored-info-boxes/
+
+Colored Info Boxes
+
+Create a component that you can reuse 10 times. This component should be able to receive props for:
+    Background Color
+    Title
+    Subtitle
+    Information
+This component should be styled and built out well.
+
+Manually pass in props for each component from a parent component.
---
+++ Assignments/Assignments/React/colored-boxes/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Component from './Component'
+
+const App = () => {
+    return (
+        <div>
+            <Component backgroundColor='blue' color='grey' textAlign='center' margin='5px' padding='5px' title='Seven' genre='Thriller' year='2000' />
+            <Component backgroundColor='grey' color='green' textAlign='center' margin='5px' padding='5px' title='Seven' genre='Thriller' year='2000' />
+            <Component backgroundColor='green' color='purple' textAlign='center' margin='5px' padding='5px' title='Seven' genre='Thriller' year='2000' />
+            <Component backgroundColor='purple' color='yellow' textAlign='center' margin='5px' padding='5px' title='Seven' genre='Thriller' year='2000' />
+            <Component backgroundColor='yellow' color='black' textAlign='center' margin='5px' padding='5px' title='Seven' genre='Thriller' year='2000' />
+            <Component backgroundColor='black' color='brown' textAlign='center' margin='5px' padding='5px' title='Seven' genre='Thriller' year='2000' />
+            <Component backgroundColor='brown' color='white' textAlign='center' margin='5px' padding='5px' title='Seven' genre='Thriller' year='2000' />
+            <Component backgroundColor='white' color='red' textAlign='center' margin='5px' padding='5px' title='Seven' genre='Thriller' year='2000' />
+            <Component backgroundColor='red' color='aqua' textAlign='center' margin='5px' padding='5px' title='Seven' genre='Thriller' year='2000' />
+            <Component backgroundColor='blue' color='grey' textAlign='center' margin='5px' padding='5px' title='Seven' genre='Thriller' year='2000' />
+        </div>
+    )
+}
+
+export default App
