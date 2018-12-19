0ccd4a700f0eb7aa66c47930a512b4ba46607b58
Scott <cadidadi@gmail.com>
2018-12-19 11:38:36 -0700

    add vschool react projects

---
+++ Assignments/React/blog-react/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './Header'
+import NavBar from './NavBar'
+import BlogList from './BlogList'
+import Footer from './Footer'
+
+const App = () => {
+    return (
+        <div>
+            <h1>hi</h1>
+            <Header />
+            <BlogList />
+            <Footer />
+        </div>
+    )
+
+}
+ 
+export default App 
